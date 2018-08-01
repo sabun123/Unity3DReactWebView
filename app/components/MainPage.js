361dce2c46742459ad7af3a8e40f04fc4e0737e1
yusuf <linkinpark_neon@yahoo.com>
2018-08-01 17:49:13 +0800

    Added input that can be passed to Unity

--- app/components/MainPage.js
+++ app/components/MainPage.js
@@ -1,27 +1,63 @@
 import React from 'react';
 import Unity, { UnityContent } from 'react-unity-webgl';
-import {Jumbotron} from 'react-bootstrap';
+import { Jumbotron, Button, FormControl } from 'react-bootstrap';
 
-class MainPage extends React.Component{
-    constructor(props){
+class MainPage extends React.Component {
+    constructor(props) {
         super(props);
 
         // Connect unity content to react
         // this paths here are relative to the index file
         this.unityContent = new UnityContent(
-            "../game_build/Build/Build.json",
+            "../game_build/Build/GameBuild.json",
             "../game_build/Build/UnityLoader.js"
+        );
+
+        this.state = {
+            messageInput: ""
+        }
+    }
+
+    sayHelloInUnity() {
+        this.unityContent.send(
+            "TextMessage",
+            "updateText",
+            this.state.messageInput
         );
     }
 
-    render(){
-        return(
+    inputChanged(e) {
+        this.setState({
+            messageInput: e.target.value
+        })
+    }
+
+    setUnityCanvasAttribute(){
+        // need to do this so that the canvas
+        // will not eat all input, at the same time will take
+        // input once focus is on it.
+        let canvas = document.getElementById("#canvas");
+        if(canvas){
+            canvas.setAttribute("tabindex","1");
+        }
+    }
+
+    render() {
+
+        this.setUnityCanvasAttribute();
+
+        return (
             <div className="container">
-            <Jumbotron style={{textAlign:"center"}}>
-                <h1>Unity3D inside ReactJS</h1>
+                <Jumbotron style={{ textAlign: "center" }}>
+                    <h1>Unity3D inside ReactJS</h1>
                 </Jumbotron>
-            <div>Unity WebGL content will go here</div>
-            <Unity unityContent={this.unityContent} />
+                <div style={{ textAlign: "center" }} >
+                    <FormControl type="text" style={{textAlign:"center"}} value={this.state.messageInput} placeholder="Tell Unity something" onChange={this.inputChanged.bind(this)} />
+                    <Button onClick={this.sayHelloInUnity.bind(this)}>Communicate with Unity</Button>
+                </div>
+                <div tabIndex="1" >
+                    <Unity unityContent={this.unityContent} />
+                </div>
             </div>
         )
     }
